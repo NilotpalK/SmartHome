c5f7c49e23554b10ed09688c2496be47dfdfd9d8
NilotpalK <nilotpalk2004@gmail.com>
2024-03-17 16:28:15 +0530

    changes5

--- src/main.jsx
+++ src/main.jsx
@@ -8,16 +8,15 @@ import CardWrapper from "./components/CardWrapper.jsx";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+    <React.StrictMode>
 
-      {/*<FanCard teamId="your_team_id" device="Fan" />*/}
-      {/*<BulbCard teamId="your_team_id" device="Bulb"/>*/}
-      {/*<LedCard teamId="your_team_id" device="Led"/>*/}
-      {/*<AcCard teamId="your_team_id" device="AC"/>*/}
-      <CardWrapper/>
+        {/*<FanCard teamId="your_team_id" device="Fan" />*/}
+        {/*<BulbCard teamId="your_team_id" device="Bulb"/>*/}
+        {/*<LedCard teamId="your_team_id" device="Led"/>*/}
+        {/*<AcCard teamId="your_team_id" device="AC"/>*/}
+        <h1 style={{textAlign: 'center', color: 'white', marginBottom:'16px'}}>Smart Home Dashboard</h1>
+        <CardWrapper/>
 
 
-
-
-  </React.StrictMode>,
+    </React.StrictMode>,
 )
